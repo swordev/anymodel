31c12fe3d9f312affd72506fe87d5ee471a2162f
Juanra GM <juanrgm724@gmail.com>
2024-09-27 08:28:16 +0200

    chore: update dependencies

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint"],
-};
